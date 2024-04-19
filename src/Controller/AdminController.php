5b420e287335710c64f860f28651f59f16dec771
sophiefaurie <faurie.sophie@educ-valadon-limoges.fr>
2024-04-19 09:08:37 +0200

    Sauvegarde du 19/04/2024

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,23 +13,98 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Entity\Produit; 
 use App\Form\ProduitType; 
+
+use Doctrine\ORM\EntityManagerInterface;
  
-class AdminController extends AbstractController 
-{   #[Route('/insert', name:'insert')] 
-    function insert(Request $request) 
-    { 
-        $produit=new Produit; 
-        $formProduit= $this->createForm(ProduitType::class,$produit); 
+class AdminController extends AbstractController {
+
+    #[Route('/insert', name:'insert')] 
+
+    // INSERER UN PRODUIT
+    function insert(Request $request, EntityManagerInterface $entityManager) { 
+        
+        $produit = new Produit; 
+        $formProduit = $this->createForm(ProduitType::class, $produit); 
+        $formProduit->add('creer', SubmitType::class, array('label' => 'Insertion d\'un produit')); 
+        $formProduit->handleRequest($request); 
  
-        $formProduit->add('creer', SubmitType::class, 
-                      array('label'=>'Insertion d\'un produit' )); 
- 
-        if($request->isMethod('post')){ 
- 
-                return new JsonResponse($request->request->all()); 
+        if ($request->isMethod('post') && $formProduit->isValid()) { 
+    
+            $file = $formProduit['lienImage']->getData(); 
+    
+            if (!is_string($file)) { 
+                $filename = $file->getClientOriginalName(); 
+                $file->move($this->getParameter('images_directory'), $filename); 
+                $produit->setLienImage($filename);
             } 
+
+            else { 
+                $session = $request->getSession(); 
+                $session->getFlashBag()->add('message', 'Vous devez choisir une image pour le produit'); 
+                $session->set('statut', 'danger'); 
+    
+                return $this->redirect($this->generateUrl('insert')); 
+            } 
+
+            $entityManager->persist($produit); 
  
-            return $this->render('Admin/create.html.twig', 
-                    array('my_form'=>$formProduit->createView())); 
+            $entityManager->flush(); 
+
+            $session=$request->getSession(); 
+            $session->getFlashBag()->add('message','un nouveau produit a été ajouté'); 
+            $session->set('statut','success'); 
+
+            return $this->redirect($this->generateUrl('liste')); 
+        }
+
+        return $this->render('Admin/create.html.twig', array('my_form'=>$formProduit->createView())); 
+    }
+    
+    #[Route('/update', name:'update')] 
+
+    // METTRE A JOUR UN PRODUIT
+    function update(Request $request, $id,EntityManagerInterface $entityManager) { 
+    
+        $produitRepository=$entityManager->getRepository(Produit::class); 
+        $produit=$produitRepository->find($id); 
+        $img=$produit->getLienImage(); 
+        $formProduit= $this->createForm(ProduitType::class,$produit);
+    
+        $formProduit->add('creer', SubmitType::class,array('label'=>'Mise à jour d\'un produit')); 
+        $formProduit->handleRequest($request); 
+    
+        if($request->isMethod('post') && $formProduit->isValid() ) {  
+
+            $file = $formProduit['lienImage']->getData(); 
+    
+            if(!is_string($file)) { 
+                $filename=$file->getClientOriginalName(); 
+                $file->move($this->getParameter('images_directory'), $filename);  
+                $produit->setLienImage($filename); 
+            } 
+                
+            else { 
+                $produit->setLienImage($img); 
+            }
+
+            $entityManager->persist($produit); 
+
+            $entityManager->flush(); 
+
+            $session=$request->getSession(); 
+            $session->getFlashBag()->add('message', 'Le produit a été mis à jour'); 
+            $session->set('statut','success'); 
+
+            return $this->redirect($this->generateUrl('liste')); 
+        }
+
+        return $this->render('Admin/create.html.twig', array('my_form'=>$formProduit->createView())); 
     } 
+
+    #[Route("/delete/{id}", name:"delete")] 
+    public function delete(Request $request,$id) 
+    { 
+ 
+    } 
+
 }
--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -1,37 +1,59 @@
 <?php 
- 
 namespace App\Form; 
  
 use App\Entity\Produit; 
 use Symfony\Component\Form\AbstractType; 
 use Symfony\Component\Form\FormBuilderInterface; 
 use Symfony\Component\OptionsResolver\OptionsResolver; 
+ 
 use Symfony\Component\Form\Extension\Core\Type\TextType; 
-use Symfony\Component\Form\Extension\Core\Type\NumberType; 
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType; 
+use Symfony\Component\Form\Extension\Core\Type\NumberType;  
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;  
 use Symfony\Component\Form\Extension\Core\Type\FileType; 
+ 
+use Symfony\Component\Form\CallbackTransformer; 
  
 class ProduitType extends AbstractType 
 { 
-    public function buildForm(FormBuilderInterface $builder, array $options) 
+    public function buildForm(FormBuilderInterface $builder, 
+array $options): void 
     { 
         $builder 
-            ->add('nom',TextType::class,array('label' => 'Nom produit :')) 
-            ->add('prix',NumberType::class,array('label' => 'Prix :')) 
-             ->add('quantite',NumberType::class, array('label' =>     'Quantité :')) 
-             ->add('rupture',CheckboxType::class, array('label'    => 'Rupture de stock ?','required' => false)) 
-             ->add('lienImage',FileType::class, array('label' => 'Image :','required' => false, 'data_class' => null, 
-'empty_data' => 'aucune image')) 
+            ->add('nom', TextType::class, array('label' => 'Nom produit :')) 
+            ->add('prix', NumberType::class, array('label' => 'Prix :')) 
+ 
+            ->add('quantite', NumberType::class, 
+                array('label' => 'Quantité :')) 
+ 
+            ->add('rupture', CheckboxType::class, 
+                array('label' => 'Rupture de stock ?', 'required' => false)) 
+ 
+            ->add('lienImage', FileType::class, 
+                array('label' => 'Image :', 'required' => false, 
+'data_class' => null, 
+                    'empty_data' => 'aucune image')) 
  
             // ->add('reference') 
             // ->add('distributeurs') 
         ; 
+        $builder->get('rupture') 
+    ->addModelTransformer(new CallbackTransformer( 
+        function ($activeAsString) { 
+            // transform the int to boolean 
+            return (bool)(int)$activeAsString; 
+        }, 
+        function ($activeAsBoolean) { 
+            // transform the boolean to string 
+            return (int)(bool)$activeAsBoolean; 
+        } 
+    )); 
+ 
     } 
-    
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Produit::class,
-        ]);
-    }
-}
+ 
+    public function configureOptions(OptionsResolver $resolver): void 
+    { 
+        $resolver->setDefaults([ 
+            'data_class' => Produit::class, 
+        ]); 
+    } 
+} 
